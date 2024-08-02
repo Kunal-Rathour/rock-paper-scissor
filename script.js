af07ae5c7549df9e788e7e9b7e2cd3dfaf552416
Kunal-Rathour <knl.rathour@gmail.com>
2024-08-02 23:53:50 +0530

    Added UI to the rps game

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-"use strict";
+// "use strict";
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissor"];
     return choice[Math.floor(Math.random() * 3)];
 }
-function getHumanChoice() {
-    return prompt("Enter your choice");
-}
-// console.log(getHumanChoice());
+// function getHumanChoice() {
+//     return prompt("Enter your choice");
+// }
+// // console.log(getHumanChoice());
 let humanScore = 0;
+const playerScoreHolder = document.querySelector("#player-score");
 let computerScore = 0;
+const computerScoreHolder = document.querySelector("#computer-score");
+const computerButton = document.querySelectorAll(".disabled");
 function playRound(humanChoice, computerChoice) {
-    playedRounds -= 1;
-    console.log(humanChoice + " " + computerChoice);
+    // console.log(humanChoice + " " + computerChoice);
     if (humanChoice == computerChoice) {
         console.log("Draw!! You chose " + humanChoice + " Computer chose " + computerChoice);
         console.log("Human Score : " + humanScore + " , Computer Score : " + computerScore);
@@ -22,36 +24,42 @@ function playRound(humanChoice, computerChoice) {
             if (computerChoice == "scissor") {
                 console.log("You Win!! Your Chose " + humanChoice + " Computer chose " + computerChoice);
                 humanScore += 1;
-                console.log("Human Score : " + humanScore + " , Computer Score : " + computerScore);
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
             }
             else {
                 console.log("You Lose!! Your Chose " + humanChoice + " Computer chose " + computerChoice);
                 computerScore += 1;
-                console.log("Human Score : " + humanScore + " , Computer Score : " + computerScore);
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
             }
         }
         else if (humanChoice == "paper") {
             if (computerChoice == "rock") {
                 console.log("You Win!! Your Chose " + humanChoice + " Computer chose " + computerChoice);
                 humanScore += 1;
-                console.log("Human Score : " + humanScore + " , Computer Score : " + computerScore);
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
             }
             else {
                 console.log("You Lose!! Your Chose " + humanChoice + " Computer chose " + computerChoice);
                 computerScore += 1;
-                console.log("Human Score : " + humanScore + " , Computer Score : " + computerScore);
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
             }
         }
         else if (humanChoice == "scissor") {
             if (computerChoice == "paper") {
                 console.log("You Win!! Your Chose " + humanChoice + " Computer chose " + computerChoice);
                 humanScore += 1;
-                console.log("Human Score : " + humanScore + " , Computer Score : " + computerScore);
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
             }
             else {
                 console.log("You Lose!! Your Chose " + humanChoice + " Computer chose " + computerChoice);
                 computerScore += 1;
-                console.log("Human Score : " + humanScore + " , Computer Score : " + computerScore);
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
             }
         }
         else {
@@ -59,11 +67,45 @@ function playRound(humanChoice, computerChoice) {
         }
     }
 }
-// let whetherToStartGame = prompt("Enter Choice : 1.Play Game 2.Exit")
-// while(whetherToStartGame!=2 || whetherToStartGame.toLowerCase()!= "exit"){
-//     playRound(getHumanChoice().toLowerCase(), getComputerChoice().toLowerCase());
-// }
-let playedRounds = 5;
-while (playedRounds > 0) {
-    playRound(getHumanChoice().toLowerCase(), getComputerChoice().toLowerCase());
-}
+const resetButton = document.querySelector(".reset");
+
+const buttons = document.querySelectorAll(".enabled");
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        const computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        const playerChoice = button.id;
+        console.log(button.id)
+        playRound(playerChoice, computerChoice);
+        if (humanScore == 5 && computerScore < 5) {
+            const result = document.querySelector(".result");
+            result.style.backgroundColor = "greenyellow"
+            result.textContent = "YOU WIN";
+            resetButton.addEventListener("click", () => {
+                result.style.backgroundColor = "";
+                result.textContent = "";
+                humanScore = 0;
+                computerScore = 0;
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
+            })
+        }
+        if (humanScore < 5 && computerScore >= 5) {
+            const result = document.querySelector(".result");
+            result.style.backgroundColor = "red"
+            result.textContent = "YOU LOSE";
+            resetButton.addEventListener("click", () => {
+                result.style.backgroundColor = "";
+                result.textContent = "";
+                humanScore = 0;
+                computerScore = 0;
+                playerScoreHolder.innerText = humanScore;
+                computerScoreHolder.innerText = computerScore;
+            })
+
+        }
+
+    })
+
+})
